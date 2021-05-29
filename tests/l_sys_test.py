d00a923a7dd22248676d700a7bcc21e77674e4dc
MrZloHex <zlo.alex.it@gmail.com>
2021-05-29 17:24:41 +0300

    ref tests

--- tests/l_sys_test.py
+++ tests/l_sys_test.py
@@ -4,30 +4,30 @@ from l_sys.l_sys import LSys
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.curve_coch = LSys(l_sys="curve_coch", iterations=2)
-        self.triang_serp = LSys(l_sys="triangle_serpinskii", iterations=3)
-        self.curve_serp = LSys(l_sys="curve_serpinskii", iterations=3)
-        self.curve_drakon = LSys(l_sys="curve_drakon", iterations=4)
-        self.pifs_tree = LSys(l_sys="pifagors_tree", iterations=3)
+        self.curve_koch = LSys(l_sys="Koch curve", iterations=2)
+        self.triangle_serp = LSys(l_sys="Sierpinski triangle", iterations=3)
+        self.curve_serp = LSys(l_sys="Sierpinski curve", iterations=3)
+        self.curve_dragon = LSys(l_sys="Dragon curve", iterations=4)
+        self.bin_tree = LSys(l_sys="Binary tree", iterations=3)
 
-    def test_CurveCoch(self):
-        result = self.curve_coch.make_seq()
+    def test_KockCurve(self):
+        result = self.curve_koch.make_seq()
         self.assertEqual(result, "f+f-f-f+f+f+f-f-f+f-f+f-f-f+f-f+f-f-f+f+f+f-f-f+f")  # noqa E501
 
-    def test_TrianglSerpin(self):
-        result = self.triang_serp.make_seq()
+    def test_TriangleSerp(self):
+        result = self.triangle_serp.make_seq()
         self.assertEqual(result, "f-g+f+g-f-gg+f-g+f+g-f+gg-f-g+f+g-f-gggg+f-g+f+g-f-gg+f-g+f+g-f+gg-f-g+f+g-f+gggg-f-g+f+g-f-gg+f-g+f+g-f+gg-f-g+f+g-f-gggggggg-gggggggg")  # noqa E501
 
-    def test_CurveSerpin(self):
+    def test_CurveSerp(self):
         result = self.curve_serp.make_seq()
         self.assertEqual(result, "b-a-b+a+b+a+b-a-b-a+b+a-b-a-b-a+b+a-b-a-b+a+b+a+b-a-b")  # noqa E501
 
-    def test_CurveDrakon(self):
-        result = self.curve_drakon.make_seq()
+    def test_CurveDragon(self):
+        result = self.curve_dragon.make_seq()
         self.assertEqual(result, "fx+yf++-fx-yf++-fx+yf+--fx-yf++-fx+yf++-fx-yf+--fx+yf+--fx-yf+")  # noqa E501
 
-    def test_PifsTree(self):
-        result = self.pifs_tree.make_seq()
+    def test_BinTree(self):
+        result = self.bin_tree.make_seq()
         self.assertEqual(result, "1111[11[1[0]0]1[0]0]11[1[0]0]1[0]0")
 
 
